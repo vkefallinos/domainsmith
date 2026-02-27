07650512c22f16bb7c38f74f2d0d9e7fb3879b61
Vasilis Kefallinos <vkefallinos@gmail.com>
2026-02-28 00:32:32 +0200

    feat: increase vertical padding in scrollable content of StudioSidebar

--- src/shell/components/StudioSidebar.tsx
+++ src/shell/components/StudioSidebar.tsx
@@ -138,7 +138,7 @@ export function StudioSidebar({
       </div>
 
       {/* Scrollable Content */}
-      <div className="flex-1 overflow-y-auto overflow-x-hidden px-3 py-4">
+      <div className="flex-1 overflow-y-auto overflow-x-hidden px-3 py-8">
         {!isCollapsed ? (
           <div className="space-y-6">
             {/* Domains Section */}
