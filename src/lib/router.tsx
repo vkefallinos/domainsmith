78cd9d0a03d77d72a9f457957d83de81561fdf55
Vasilis Kefallinos <vkefallinos@gmail.com>
2026-02-28 12:11:01 +0200

    feat: remove unused data shape, design system, product, section, and shell loaders

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,12 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { ProductPage } from '@/components/ProductPage'
-import { DataShapePage } from '@/components/DataShapePage'
-import { DesignPage } from '@/components/DesignPage'
-import { SectionsPage } from '@/components/SectionsPage'
-import { SectionPage } from '@/components/SectionPage'
-import { ScreenDesignPage, ScreenDesignFullscreen } from '@/components/ScreenDesignPage'
-import { ShellDesignPage, ShellDesignFullscreen } from '@/components/ShellDesignPage'
-import { ExportPage } from '@/components/ExportPage'
 import LandingLayout from '@/shell/LandingLayout'
 import ShellLayout from '@/shell/ShellLayout'
 import StudioLayout from '@/shell/StudioLayout'
@@ -46,41 +38,5 @@ export const router = createBrowserRouter([
       // Agent flow editing view (shows modal)
       { path: 'agent/:agentId/commands/:commandId' },
     ],
-  },
-  {
-    path: '/data-shape',
-    element: <DataShapePage />,
-  },
-  {
-    path: '/design',
-    element: <DesignPage />,
-  },
-  {
-    path: '/sections',
-    element: <SectionsPage />,
-  },
-  {
-    path: '/sections/:sectionId',
-    element: <SectionPage />,
-  },
-  {
-    path: '/sections/:sectionId/screen-designs/:screenDesignName',
-    element: <ScreenDesignPage />,
-  },
-  {
-    path: '/sections/:sectionId/screen-designs/:screenDesignName/fullscreen',
-    element: <ScreenDesignFullscreen />,
-  },
-  {
-    path: '/shell/design',
-    element: <ShellDesignPage />,
-  },
-  {
-    path: '/shell/design/fullscreen',
-    element: <ShellDesignFullscreen />,
-  },
-  {
-    path: '/export',
-    element: <ExportPage />,
   },
 ])
