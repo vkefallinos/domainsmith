dfd8c4223036f45a8cad780b2bc2819f092f267f
Vasilis Kefallinos <vkefallinos@gmail.com>
2026-03-01 15:37:11 +0200

    refactor: Remove deprecated chat-related components and clean up index exports

--- src/shell/components/index.ts
+++ src/shell/components/index.ts
@@ -5,9 +5,3 @@ export type { StudioShellProps } from './StudioShell'
 // Shell sub-components
 export { StudioSidebar } from './StudioSidebar'
 export type { StudioSidebarProps } from './StudioSidebar'
-
-// Legacy components (may be deprecated)
-export { MainNav } from './MainNav'
-export { UserMenu } from './UserMenu'
-export type { NavigationItem, MainNavProps } from './MainNav'
-export type { UserMenuProps } from './UserMenu'
